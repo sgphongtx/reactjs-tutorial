c5acc679c2c82849c83c562653d43f3b020e393a
PhongTX <phong.tran@namlongsoft.net>
2021-10-16 08:44:53 +0700

    Update Api

--- src/core/utils/Api.js
+++ src/core/utils/Api.js
@@ -10,6 +10,34 @@ export const clientQuery = async params => {
     const url = params.url ? params.url : ''
     const method = params.method ? params.method : 'GET'
     const data = params.data ? params.data : {}
+
+    const onSuccess = response => {
+        if (response.status === HTTP.EXPIRED_TOKEN) {
+            // remove token and redirect to login page the page for them
+            return Promise.reject({ message: 'Please re-authenticate.' })
+        } else {
+            return response.data
+        }
+    }
+
+    const onError = error => {
+        console.error('Request Failed:', error.config)
+
+        if (error.response) {
+            // Request was made but server responded with something
+            // other than 2xx
+            console.error('Status:', error.response.status)
+            console.error('Data:', error.response.data)
+            console.error('Headers:', error.response.headers)
+        } else {
+            // Something else happened while setting up the request
+            // triggered the error
+            console.error('Error Message:', error.message)
+        }
+
+        return Promise.reject(error.response || error.message)
+    }
+
     return await axios({
         // `method` is the request method to be used when making the request
         method: method.toLowerCase(),
@@ -23,23 +51,13 @@ export const clientQuery = async params => {
         headers: {
             'X-Requested-With': 'XMLHttpRequest',
             Accept: 'application/json',
-            Authorization: token
+            Authorization: `Bearer ${token}`
         },
         // `timeout` specifies the number of milliseconds before the request times out.
         // If the request takes longer than `timeout`, the request will be aborted.
         timeout: 1000, // default is `0` (no timeout)
         data: data
     })
-        .then(response => {
-            if (response.status === HTTP.EXPIRED_TOKEN) {
-                // remove token and redirect to login page the page for them
-                return Promise.reject({ message: 'Please re-authenticate.' })
-            } else {
-                return response.data
-            }
-        })
-        .catch(error => {
-            // handle error
-            console.log(error)
-        })
+        .then(onSuccess)
+        .catch(onError)
 }
